41d17f9cc2eeea01a43b4fb04fdb432aec50355f
Bruno Fernandes <brunoxrfernandes@gmail.com>
2026-01-19 22:42:46 +0000

    Backlog and Entity

--- src/main/java/com/example/finwise/application/controller/RegisterController.java
+++ src/main/java/com/example/finwise/application/controller/RegisterController.java
@@ -1,4 +1,15 @@
 package com.example.finwise.application.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class RegisterController {
+
+    @RequestMapping("/register")
+    @ResponseBody
+    public String registerUser(){
+        return "register";
+    }
 }
--- src/main/java/com/example/finwise/infraestructure/persistence/entity/UserEntity.java
+++ src/main/java/com/example/finwise/infraestructure/persistence/entity/UserEntity.java
@@ -1,4 +1,9 @@
 package com.example.finwise.infraestructure.persistence.entity;
 
 public class UserEntity {
+    private String  firstName;
+    private String lastName;
+    private String username;
+    private String email;
+    private String password;
 }
